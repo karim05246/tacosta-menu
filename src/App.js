7b56a669e48f7d04ff12d51e494f09bf4c301db2
karim05246 <medkarimelbourai@gmail.com>
2025-10-03 22:26:50 +0100

    i change the title of the pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>☕ Tacos'Ta Menu </h1>
+        <h1>☕ Tacos'Ta Menu 🌮</h1>
   <p> Helloo! Enjoy the cozy vibes of our café and the best drinks with us.</p>
       
     
